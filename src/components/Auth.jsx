181f87d9d812d019b5e269f3710431eedcd2ca66
sargethefrog <sargethefrog@gmail.com>
2021-04-16 23:55:42 +0300

    Реализован компонент Авторизация

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {Redirect} from "react-router-dom";
 
 export class Auth extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {};
+        this.handlerSubmit = this.handlerSubmit.bind(this);
+    }
+
+    handlerSubmit(e){
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        fetch('http://y91756wn.beget.tech/imagegallery/php/handlerAuth.php',{
+            method : 'POST',
+            body : formData
+        }).then(response => response.json())
+            .then(result => {
+                if(result.result === 'success'){
+                    this.setState({info : <Redirect to="/" />})
+                } else {
+                    this.setState({info : <div id="info" className="error">Неверный логин и / или пароль.</div>})
+                }
+            });
+    }
     render(){
         return (
             <div className="container">
                 <h1 className="text-center">Авторизация</h1>
-                <form id="authForm">
+                {this.state.info}
+                <form id="authForm" onSubmit={this.handlerSubmit}>
                     <input type="text" name="email" className="form-control my-3" placeholder="E-mail" required />
                         <input type="password" name="pass" className="form-control my-3" placeholder="Пароль" required />
                             <input type="submit" name="auth_submit" className="form-control btn btn-primary my-3"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,82 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-export function Header(){
-    return (
-        <header className="container my-3 pb-3">
-            <div className="row">
-                <div id="logo" className="col-md-8 mb-3">
-                    <Link to="/">
-                        <img src="img/logo.png" />
-                            <span>Галерея изображений</span>
-                    </Link>
-                </div>
-                <div className="reg_and_auth col-md-4">
-                    <Link to="/reg" className="btn ms-2">
-                        <i className="fa fa-address-book" aria-hidden="true"></i>
-                        <span className="visible-md">Регистрация</span></Link>
-                    <Link to="/auth" className="btn">
-                        <i className="fa fa-sign-in-alt" aria-hidden="true"></i>
-                        Вход
-                    </Link>
-                    <span className="mx-2">
-            <i className="fas fa-user"></i>
+
+function AuthPanel(props){
+    if(props.loggedIn) return (
+        <div className="reg_and_auth col-md-4">
+            <span className="mx-2">
+            <i className="fas fa-user"></i>{props.user}
         </span>
-                    <a href="/logout" className="btn">
-                        <i className="fa fa-sign-out-alt" aria-hidden="true"></i>
-                        Выход
-                    </a>
-                </div>
-            </div>
-        </header>
+            <Link to="/logout" className="btn">
+                <i className="fa fa-sign-out-alt" aria-hidden="true"></i>
+                Выход
+            </Link>
+        </div>
+    ); else return (
+        <div className="reg_and_auth col-md-4">
+            <Link to="/reg" className="btn ms-2">
+                <i className="fa fa-address-book" aria-hidden="true"></i>
+                <span className="visible-md">Регистрация</span></Link>
+            <Link to="/auth" className="btn">
+                <i className="fa fa-sign-in-alt" aria-hidden="true"></i>
+                Вход
+            </Link>
+        </div>
     );
+}
+
+/*class AuthPanel extends React.Component{
+    render(){
+
+    }
+}*/
+
+export class Header extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {};
+        this.state.name = '';
+    }
+
+    componentDidMount(){
+        fetch('http://y91756wn.beget.tech/imagegallery/php/getUser.php',{
+            credentials : "include"
+        })
+            .then(response => response.json())
+            .then(result => {
+                this.setState({
+                    loggedIn : true,
+                    userId : result.id,
+                    name : result.name,
+                    email : result.email
+                })
+            });
+    }
+
+    logout(){
+        fetch('http://y91756wn.beget.tech/imagegallery/php/handlerLogout.php')
+            .then(response => response.json())
+            .then(result => {
+                if(result.result === 'success'){
+                    this.setState({loggedIn : false});
+                }
+            });
+    }
+
+    render(){
+        return (
+            <header className="container my-3 pb-3">
+                <div className="row">
+                    <div id="logo" className="col-md-8 mb-3">
+                        <Link to="/">
+                            <img src="img/logo.png" />
+                            <span>Галерея изображений</span>
+                        </Link>
+                    </div>
+                    <AuthPanel loggedIn={this.state.loggedIn} user={this.state.name}/>
+                </div>
+            </header>
+        );
+    }
 }
